0241e0056dd69d01f06eeb293e99d6aa43c1fac0
Francisco Jacildo Furtado da Silva <102054158+jacildofurtado@users.noreply.github.com>
2023-06-01 15:43:52 -0300

    Study day

--- ftont-end-completo/Introduzindo-JQuery/js/functions.js
+++ ftont-end-completo/Introduzindo-JQuery/js/functions.js
@@ -20,4 +20,36 @@ $(function(){
             e.stopPropagation();
         });
     }
+
+    $('form#form1').submit(function(e){
+        e.preventDefault();
+        var nome = $('input[name=nome]').val();
+        var telefone = $('input[name=telefone]').val();
+        var email = $('input[name=email]').val();
+        
+        //Contando a quantidade de espaços e os respectivos valores.
+        var amount = nome.split(' ').length;
+        var splitStr = nome.split(' ');
+        if(amount >= 2){
+            for(var i = 0; i < amount; i++){
+                if(splitStr[i].match(/^[A-Z]{1}[a-z]{1,}$/)){
+
+                }else{
+                    aplicarCampoInvalido($('input[name=nome]'));
+                    return false;
+                }
+            }
+        }else{
+            aplicarCampoInvalido($('input[name=nome]'));
+            return false;
+        }
+
+        //Se chegou até o final é por que está tudo okay!
+    })
+
+    function aplicarCampoInvalido(el){
+        el.css('border', '2px solid red');
+        el.data('invalido', 'true');
+        el.val('Campo Inválido');
+    }
 })
