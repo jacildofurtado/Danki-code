ef38eb2e1a392b522a7e59fc9b89083174d61196
Francisco Jacildo Furtado da Silva <102054158+jacildofurtado@users.noreply.github.com>
2023-06-02 11:27:41 -0300

    study

--- ftont-end-completo/Introduzindo-JQuery/js/functions.js
+++ ftont-end-completo/Introduzindo-JQuery/js/functions.js
@@ -1,4 +1,5 @@
 $(function(){
+    //funções de abrir e fechar formulário
     abrirJanela();
     verificarCliqueFechar();
 
@@ -21,35 +22,62 @@ $(function(){
         });
     }
 
+    //Eventos do formulário
+    $('input[type=text]').focus(function(){
+        resetarCampoInvalido($(this));
+    });
+
     $('form#form1').submit(function(e){
         e.preventDefault();
         var nome = $('input[name=nome]').val();
         var telefone = $('input[name=telefone]').val();
         var email = $('input[name=email]').val();
         
+        if(verificarNome(nome) == false){
+            aplicarCampoInvalido($('input[name=nome]'));
+        }else{
+            alert("Formulário enviado com sucesso!");
+        }
+
+
+        //Se chegou até o final é por que está tudo okay!
+    });
+
+    
+    //funções para estilizar o campo do formulário
+
+    function aplicarCampoInvalido(el){
+        el.css('color', 'red')
+        el.css('border', '2px solid red');
+        //el.data('invalido', 'true');
+        el.val('Campo Inválido');
+    }
+
+    function resetarCampoInvalido(el){
+        el.css('color', '#ccc')
+        el.css('border', '1px solid #ccc');
+        //el.data('invalido', 'true');
+        el.val('');
+    }
+
+    //funçòes para verificar os nossos campos
+    function verificarNome(nome){
         //Contando a quantidade de espaços e os respectivos valores.
+        if(nome == ''){
+            return false;
+        }
         var amount = nome.split(' ').length;
         var splitStr = nome.split(' ');
         if(amount >= 2){
             for(var i = 0; i < amount; i++){
                 if(splitStr[i].match(/^[A-Z]{1}[a-z]{1,}$/)){
-
+        
                 }else{
-                    aplicarCampoInvalido($('input[name=nome]'));
-                    return false;
+                     return false;
                 }
             }
         }else{
-            aplicarCampoInvalido($('input[name=nome]'));
             return false;
         }
-
-        //Se chegou até o final é por que está tudo okay!
-    })
-
-    function aplicarCampoInvalido(el){
-        el.css('border', '2px solid red');
-        el.data('invalido', 'true');
-        el.val('Campo Inválido');
     }
 })
