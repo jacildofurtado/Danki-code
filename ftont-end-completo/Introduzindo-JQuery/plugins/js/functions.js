8e8bb87bdba51a02a2026da14c2400349fc2a2de
Francisco Jacildo Furtado da Silva <102054158+jacildofurtado@users.noreply.github.com>
2023-06-20 10:38:54 -0300

    att

--- ftont-end-completo/Introduzindo-JQuery/plugins/js/functions.js
+++ ftont-end-completo/Introduzindo-JQuery/plugins/js/functions.js
@@ -1,9 +1,13 @@
 $(function(){
-
+    //Plugin social media
     $('#social-share').jsSocials({
         shares: ["twitter", "facebook", "googleplus", "linkedin", "pinterest", "stumbleupon", "whatsapp"]
     });
 
+    //Plugin mask
+    $('input[name=telefone]').mask('(99)99999-9999');
+    $('input[name=data]').mask('99/99/9999');
 
-
+    //Plugin Fancybox
+    $('a').fancybox();
 })
