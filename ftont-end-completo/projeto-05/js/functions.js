0c3f74dafd0c46aa95184f519ccc995d01157997
Francisco Jacildo Furtado da Silva <102054158+jacildofurtado@users.noreply.github.com>
2023-07-12 10:00:01 -0300

    add slider effect

--- ftont-end-completo/projeto-05/js/functions.js
+++ ftont-end-completo/projeto-05/js/functions.js
@@ -1,4 +1,8 @@
 $(function(){
+
+    /*
+        sistema de pesquisa
+    */
     var currentValue = 0;
     var isDrag = false;
     var maxPrice = 70000;
@@ -68,4 +72,68 @@ $(function(){
         $('body').css('-o-user-select', 'auto');
         $('body').css('user-select', 'auto');
     }
+
+    /*
+      sistema de slide da paggina individual de cada carro
+    */
+
+    var imgShow = 3;
+    var maxIndex = Math.ceil($('.mini-img-wraper').length/3) - 1;
+    var curIndex = 0;
+
+    initSlider();
+    navigateSlider();
+    clickSlider();
+    function initSlider(){
+        var amt = $('.mini-img-wraper').length * 33.3;
+        var elScroll = $('.nav-galeria-wraper');
+        var elSingle = $('.mini-img-wraper');
+        elScroll.css('width', amt+'%');
+        elSingle.css('width', 33.3*(100/amt)+'%');
+    }
+
+    function navigateSlider(){
+        $('.arrow-right-nav').click(function(){
+            if(curIndex < maxIndex){
+                curIndex++;
+                var elOff = $('.mini-img-wraper').eq(curIndex*3).offset().left - $('.nav-galeria-wraper').offset().left;
+                $('.nav-galeria').animate({'scrollLeft':elOff+'px'});
+            }else{
+                //console.log('Chegamos até o final')
+            }
+        });
+
+        $('.arrow-left-nav').click(function(){
+            if(curIndex > 0){
+                curIndex--;
+                var elOff = $('.mini-img-wraper').eq(curIndex*3).offset().left - $('.nav-galeria-wraper').offset().left;
+                $('.nav-galeria').animate({'scrollLeft':elOff+'px'});
+            }else{
+                //console.log('Chegamos até o final')
+            }
+        });
+    }
+
+    function clickSlider(){
+        $('.mini-img-wraper').click(function(){
+            $('.mini-img-wraper').css('background-color', 'transparent');
+            $(this).css('background-color','rgb(210,210,210');
+            var img = $(this).children().css('background-image');
+            $('.foto-destaque').css('background-image',img);
+        });
+
+        $('.mini-img-wraper').eq(0).click();
+    }
+
+
+    /*
+    Clicar e ir para div de contato com base no atributo goto
+    */
+
+    $('[goto=contato]').click(function(){
+        $('nav a').css('color','black');
+        $(this).css('color','#EB2D2D');
+        $('html,body').animate({'scrollTop':$('#contato').offset().top});
+        return false;
+    })
 })
